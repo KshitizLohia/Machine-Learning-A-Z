968bdbb275042330427063d906a9d214ab44a8d5
Kshitiz Lohia <kshitiz.lohia@inmobi.com>
2018-01-09 12:51:48 +0530

    Regression models

---
+++ Part 1 - Data Preprocessing/1.datapreprocessing.R
@@ -0,0 +1,49 @@
+# Data Preproceesing
+
+# Import dataset
+dataset = read.csv('Data.csv')
+
+# Remove missing values via mean
+dataset$Age = ifelse(is.na(dataset$Age), 
+                     ave(dataset$Age, FUN = function(x) mean(x,na.rm = TRUE)),
+                     dataset$Age)
+dataset$Salary = ifelse(is.na(dataset$Salary), 
+                     ave(dataset$Salary, FUN = function(x) mean(x,na.rm = TRUE)),
+                     dataset$Salary)
+
+# Encoding categorical varibles
+dataset$Country = factor(dataset$Country, 
+                         levels = c('France','Spain','Germany'),
+                         labels = c(1,2,3)
+                         )
+
+dataset$Purchased = factor(dataset$Purchased, 
+                         levels = c('No','Yes'),
+                         labels = c(0,1)
+)
+
+# Splitting the data in training set and test set
+install.packages('caTools')
+library(caTools)
+set.seed(123)
+split = sample.split(dataset$Purchased, SplitRatio = 0.8)
+training_set = subset(dataset, split==TRUE)
+test_set = subset(dataset, split==FALSE)
+
+
+# Feature Scaling
+training_set[,2:3] = scale(training_set[,2:3])
+test_set[,2:3] = scale(test_set[,2:3])
+
+
+
+
+
+
+
+
+
+
+
+                   
+
