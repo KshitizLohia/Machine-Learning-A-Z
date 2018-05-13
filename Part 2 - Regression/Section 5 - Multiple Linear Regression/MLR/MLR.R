89a7a57f5883f21780f4f9b120f77e54806ab9b6
Kshitiz Lohia <kshitiz.lohia@inmobi.com>
2018-05-13 19:26:24 +0530

    Fast AI

---
+++ Part 2 - Regression/Section 5 - Multiple Linear Regression/MLR/MLR.R
@@ -0,0 +1,15 @@
+# Preprocessing
+dataset = read.csv('50_Startups.csv')
+
+
+dataset$State = factor(dataset$State, 
+                         levels = c('New York','California','Florida'),
+                         labels = c(1,2,3)
+)
+
+install.packages('caTools')
+library(caTools)
+set.seed(123)
+split = sample.split(dataset$Profit, SplitRatio = 0.8)
+training_set = subset(dataset, split==TRUE)
+test_set = subset(dataset, split==FALSE)
